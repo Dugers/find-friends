a98d2d8d838ff76671e1a38108b3ca3cb9763916
unknown <opka4444@gmail.com>
2022-07-17 16:21:16 +0700

    Fix none username

--- middlewares/users.py
+++ middlewares/users.py
@@ -1,10 +1,14 @@
-from aiogram.dispatcher.middlewares import BaseMiddleware
-from aiogram.types import Message, CallbackQuery, Update
 from utils.db import get_users, update_user
+from aiogram.types import Message, CallbackQuery
+from aiogram.dispatcher.middlewares import BaseMiddleware
+from aiogram.dispatcher.handler import CancelHandler
 
 
 class UserRoleMiddleware(BaseMiddleware):
     async def on_pre_process_message(self, message: Message, data):
+        if message.from_user.username is None:
+            await message.answer("Для использования бота вам нужно иметь username в телеграм\nДля этого зайдите в настройки -> \"Изменить профиль\" -> \"Имя пользователя\"\nЗатем запишите туда любое имя")
+            raise CancelHandler()
         data['user_info'] = await get_users(one=True, telegram_id=message.from_user.id)
         try:
             if data['user_info']['username'] != message.from_user.username:
@@ -13,6 +17,9 @@ class UserRoleMiddleware(BaseMiddleware):
             pass
 
     async def on_pre_process_callback_query(self, callback: CallbackQuery, data):
+        if callback.from_user.username is None:
+            await callback.message.answer("Для использования бота вам нужно иметь username в телеграм\nДля этого зайдите в настройки -> \"Изменить профиль\" -> \"Имя пользователя\"\nЗатем запишите туда любое имя")
+            raise CancelHandler()
         data['user_info'] = await get_users(one=True, telegram_id=callback.from_user.id)
         try:
             if data['user_info']['username'] != callback.from_user.username:
